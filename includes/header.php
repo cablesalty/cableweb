e6c7ac0c4c6be35f56e6eb80f7fc0eec34c52c6f
cablesalty <cablesalty@gmail.com>
2024-05-11 21:39:01 +0200

    Use include to include the header instead of duplication

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/main.css">
+    <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
+    <title>cablesalty</title>
+</head>
+
+<div class="header">
+    <h1 class="title">cablesalty</h1>
+    <h2 class="subtitle">under construction...</h2>
+    <div class="navbar">
+        <a href="/">Homepage</a>
+        <a href="/serverinfo">Server info</a>
+        <a href="https://github.com/cablesalty">Github</a>
+        <a href="">Discord</a>
+        <a href="https://github.com/cablesalty/cableweb">View source code</a>
+    </div>
+</div>
