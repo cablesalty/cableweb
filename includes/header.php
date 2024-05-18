a2aa1eb62958241bedbd2b68ce3e76d27246cac0
cablesalty <cablesalty@gmail.com>
2024-05-18 14:50:47 +0200

    Update title

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
     <a href="javascript:void(0)" id="mobilemenuicon"><i class="fa-solid fa-bars"></i></a>
     <a href="/cpanel/index.php" class="cpanelicon"><i class="fa-solid fa-user-tie"></i></a>
     <h1 class="title">cablesalty</h1>
-    <h2 class="subtitle">under construction...</h2>
+    <h2 class="subtitle">welcome back!</h2>
     <div class="navbar" id="navbar">
         <a href="/">Homepage</a>
         <a href="/serverinfo">Server info</a>
